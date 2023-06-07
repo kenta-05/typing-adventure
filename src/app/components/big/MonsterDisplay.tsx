7d8f4ce9dce535d193acdd758d33f1c941a4c76a
sotetu-79 <sotetu79@gmail.com>
2023-06-07 21:53:51 +0900

    敵のHPバーの変動を追加

--- src/app/components/big/MonsterDisplay.tsx
+++ src/app/components/big/MonsterDisplay.tsx
@@ -19,7 +19,9 @@ function MonsterDisplay({ monster, monsterHp }: Props) {
           <div className="w-56 mb-3 h-6 bg-slate-200 rounded-sm">
             <div
               className="flex items-center h-6 bg-red-600 rounded-sm text-2xl font-bold pl-1"
-              style={{ width: `${100}%` }}
+              style={{
+                width: `${monster ? (monsterHp / monster.hp) * 100 : 0}%`,
+              }}
             >
               <p className="text-white">{monsterHp}</p>
             </div>
