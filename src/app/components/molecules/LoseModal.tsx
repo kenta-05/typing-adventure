557b0cacc77f651362ae9ab9c2c202b7ac29acf7
sotetu-79 <sotetu79@gmail.com>
2023-06-17 17:51:25 +0900

    敗北時モーダルの修正

--- src/app/components/molecules/LoseModal.tsx
+++ src/app/components/molecules/LoseModal.tsx
@@ -17,7 +17,7 @@ function loseModal({
 }: Props) {
   return (
     <div className="bg-gray-500 bg-opacity-50 absolute inset-0 flex items-center justify-center z-10">
-      <div className="rounded-xl p-4 w-96 flex flex-col justify-center items-center bg-slate-300">
+      <div className="rounded-xl p-4 w-[28rem] flex flex-col justify-center items-center bg-base">
         <p className="text-4xl font-bold overflow-y-hidden">タイピング終了</p>
         <div className="flex items-end mt-2">
           <p className="text-[1.5rem] font-bold text-red-500">
@@ -25,30 +25,40 @@ function loseModal({
           </p>
           <p className="text-[1.5rem]">に負けました…</p>
         </div>
-        <div className="bg-gray-400 p-2 w-full rounded-sm mt-4">
-          <p className="text-[1.5rem] mb-4 font-bold">スコア:{currentType}</p>
-          <p className="text-lg">正確なタイプ数:{currentType}</p>
-          <p className="text-lg">ミスタイプ数:{wrongType}</p>
+        <div className="bg-second p-2 w-full rounded-sm mt-4 flex justify-between items-end">
+          <div>
+            <p className="text-[1.5rem] mb-4 font-bold">スコア:{currentType}</p>
+            <p className="text-lg">正確なタイプ数:{currentType}</p>
+            <p className="text-lg">ミスタイプ数:{wrongType}</p>
+          </div>
+          <img
+            className="w-24 h-24 mr-3"
+            src={`/opponents/${prevMonster?.image}.png`}
+            alt="モンスター"
+          />
         </div>
-        <div className="mt-6 space-x-7">
+        <div className="mt-6 space-x-12">
           <Link href="/">
             <button
               onClick={() => {
                 setLoseModal(false);
               }}
-              className="bg-gray-400 p-2 rounded-sm transition-all hover:bg-gray-500"
+              className="bg-first p-2 rounded-sm transition-all 
+              hover:bg-third"
             >
-              ホーム画面
+              ホーム画面へ
             </button>
           </Link>
+          <button
+            onClick={() => {
+              window.location.reload();
+            }}
+            className="bg-first px-8 py-2 rounded-sm transition-all 
+              hover:bg-third"
+          >
+            完了
+          </button>
         </div>
-      </div>
-      <div className="absolute left-10">
-        <img
-          className="w-48"
-          src={`/opponents/${prevMonster?.image}.png`}
-          alt="モンスター"
-        />
       </div>
     </div>
   );
