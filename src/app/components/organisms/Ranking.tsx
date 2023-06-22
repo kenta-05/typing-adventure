77f0f6a05678467d46d834416f79565f597e15f5
sotetu-79 <sotetu79@gmail.com>
2023-06-23 07:58:40 +0900

    ランキングのローディング表示追加

--- src/app/components/organisms/Ranking.tsx
+++ src/app/components/organisms/Ranking.tsx
@@ -14,6 +14,7 @@ import { GameUser } from "@/app/types/primary";
 
 function Ranking() {
   const [users, setUsers] = useState<GameUser[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const db = getFirestore();
@@ -29,6 +30,7 @@ function Ranking() {
         newUsers.push(doc.data() as GameUser);
       });
       setUsers(newUsers);
+      setLoading(false);
     });
 
     // クリーンアップ↓
@@ -38,6 +40,22 @@ function Ranking() {
   return (
     <div className="max-h-[30.1rem] min-w-full">
       <div className="bg-first text-black">ランキング</div>
+      {loading && (
+        // <div
+        //   className="flex justify-center h-screen pt-16"
+        //   aria-label="読み込み中"
+        // >
+        //   <div className="animate-ping h-3 w-3 bg-first rounded-full"></div>
+        //   <div className="animate-ping h-3 w-3 bg-first rounded-full mx-7"></div>
+        //   <div className="animate-ping h-3 w-3 bg-first rounded-full"></div>
+        // </div>
+        <div
+          className="flex justify-center h-screen pt-4"
+          aria-label="読み込み中"
+        >
+          <div className="animate-spin h-16 w-16 border-[0.45rem] border-first rounded-full border-t-transparent"></div>
+        </div>
+      )}
       {users.map((user, index) => (
         <RankPlayer user={user} index={index} key={user.uid} />
       ))}
