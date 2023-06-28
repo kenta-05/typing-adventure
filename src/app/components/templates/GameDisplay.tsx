f2b6aa637895f0c44a4880c242428500c64e763b
sotetu-79 <sotetu79@gmail.com>
2023-06-28 14:10:08 +0900

    画像の事前読み込みを追加
    モンスターやアイテムを含めた画像 を事前読 み込みすることで、快適なプレイに

--- src/app/components/templates/GameDisplay.tsx
+++ src/app/components/templates/GameDisplay.tsx
@@ -126,30 +126,6 @@ function GameDisplay() {
     Howler.autoUnlock = true;
     isSoundRef.current = isSound;
 
-    // 画像の読み込み
-    const imageUrls = [
-      "/background/stage-desert.jpg",
-      "/background/stage-desert-dark.jpg",
-      "/background/stage-factory.jpg",
-      "/background/stage-factory-dark.jpg",
-      "/background/stage-factory-light.jpg",
-      "/background/stage-frozen.jpg",
-      "/background/stage-frozen-dark.jpg",
-      "/background/stage-ocean.jpg",
-      "/background/stage-plains.jpg",
-      "/background/stage-swaps.jpg",
-      "/background/stage-swaps-dark.jpg",
-      "/background/stage-universe.jpg",
-      "/background/stage-volcano.jpg",
-      "/background/stage-volcano-dark.jpg",
-    ];
-
-    // すべての画像をプリロード
-    imageUrls.forEach((url) => {
-      const img = new Image();
-      img.src = url;
-    });
-
     // HPが0以下になるとgame_stop()
     if (hp <= 0) {
       game_stop();
@@ -547,6 +523,86 @@ function GameDisplay() {
     3000
   );
 
+  const fetchImage = () => {
+    // 画像の読み込み
+    const imageUrls = [
+      "/background/stage-desert.jpg",
+      "/background/stage-desert-dark.jpg",
+      "/background/stage-factory.jpg",
+      "/background/stage-factory-dark.jpg",
+      "/background/stage-factory-light.jpg",
+      "/background/stage-frozen.jpg",
+      "/background/stage-frozen-dark.jpg",
+      "/background/stage-ocean.jpg",
+      "/background/stage-plains.jpg",
+      "/background/stage-swaps.jpg",
+      "/background/stage-swaps-dark.jpg",
+      "/background/stage-universe.jpg",
+      "/background/stage-volcano.jpg",
+      "/background/stage-volcano-dark.jpg",
+
+      "/opponents/baranbiruda.png",
+      "/opponents/bi.png",
+      "/opponents/bomuzikiru.png",
+      "/opponents/buzerere.png",
+      "/opponents/garagara.png",
+      "/opponents/griffon.png",
+      "/opponents/kione.png",
+      "/opponents/kodora.png",
+      "/opponents/korio.png",
+      "/opponents/kureina.png",
+      "/opponents/miku.png",
+      "/opponents/moegame.png",
+      "/opponents/odoshio.png",
+      "/opponents/pimo.png",
+      "/opponents/question.png",
+      "/opponents/ririppo.png",
+      "/opponents/sentori.png",
+      "/opponents/seziru.png",
+      "/opponents/siromaro.png",
+      "/opponents/slime.png",
+      "/opponents/temi_ru.png",
+      "/opponents/tokotoko.png",
+      "/opponents/torubo.png",
+      "/opponents/wyvern.png",
+      "/opponents/wyvernChild.png",
+      "/opponents/yakarabati.png",
+      "/opponents/zakiraru.png",
+
+      "/sounds/cure.mp3",
+      "/sounds/find.mp3",
+      "/sounds/missType.mp3",
+      "/sounds/monsterAttack.mp3",
+      "/sounds/start.mp3",
+      "/sounds/text.mp3",
+      "/sounds/type.mp3",
+
+      "/item/bronze_sword.png",
+      "/item/buzerere_piece.png",
+      "/item/cocktail_blue.png",
+      "/item/crystal.png",
+      "/item/fire_blue.png",
+      "/item/fire_red.png",
+      "/item/iron_sword.png",
+      "/item/light.png",
+      "/item/milk.png",
+      "/item/ocean_sword.png",
+      "/item/portion_blue.png",
+      "/item/portion_green.png",
+      "/item/portion_red.png",
+      "/item/portion_special.png",
+      "/item/star.png",
+      "/item/thunder.png",
+      "/item/yumi.png",
+    ];
+
+    // すべての画像をプリロード
+    imageUrls.forEach((url) => {
+      const img = new Image();
+      img.src = url;
+    });
+  };
+
   // ゲーム進行(f)
   const game_start = async () => {
     setPlaying(true);
@@ -561,6 +617,7 @@ function GameDisplay() {
         console.error("Failed to play the start sound: ", error);
       }
     }
+    fetchImage();
 
     await write("冒険の始まりだ");
     await write("戦闘の勝敗はタイピング力によって決まる！");
