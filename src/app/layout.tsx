67dca27b4d9c743e253c0b525627a255316239ca
sotetu-79 <sotetu79@gmail.com>
2023-07-02 13:38:38 +0900

    メタデータの記述方法の変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,6 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <Head>
-        <meta
-          name="twitter:image"
-          content="https://typing-adventure.com/main/twitter.png"
-        />
-        <meta
-          property="og:image"
-          content="https://typing-adventure.com/main/opengraph.png"
-        />
-      </Head>
       <body
         className={`${inter.className} bg-main shadow-lg min-h-screen w-full flex justify-center`}
       >
