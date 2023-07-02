f387661c1e6ca573d4849f5cff1b5b45e9a67025
sotetu-79 <sotetu79@gmail.com>
2023-07-02 19:36:43 +0900

    bodyタグを除去

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,13 +37,48 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <body
+      <Head>
+        <title>タイピング冒険記</title>
+        <meta
+          name="description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta
+          name="keywords"
+          content="タイピングゲーム, タイピング練習, 対戦, 冒険, 無料"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="タイピング冒険記" />
+        <meta
+          property="og:description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta property="og:site_name" content="タイピング冒険記" />
+        <meta property="og:url" content="https://typing-adventure.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="タイピング冒険記" />
+        <meta
+          name="twitter:description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta name="twitter:site" content="@sotetu79" />
+        <meta name="twitter:creator" content="@sotetu79" />
+        <meta
+          name="twitter:image"
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}/twitter.png`}
+        />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}/opengraph.png`}
+        />
+      </Head>
+      <div
         className={`${inter.className} bg-main shadow-lg min-h-screen w-full flex justify-center`}
       >
         <FirebaseProvider>
           <div className="w-320">{children}</div>
         </FirebaseProvider>
-      </body>
+      </div>
     </>
   );
 }
