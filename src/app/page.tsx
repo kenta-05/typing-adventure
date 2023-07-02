9305bcdc7789e700fc632efc361e828b620a1848
sotetu-79 <sotetu79@gmail.com>
2023-07-02 19:29:09 +0900

    Headの移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,46 @@ import HomeDisplay from "./components/templates/HomeDisplay";
 import Developer from "./components/organisms/Developer";
 import Article from "./components/organisms/Article";
 import ShareSns from "./components/organisms/ShareSns";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>タイピング冒険記</title>
+        <meta
+          name="description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta
+          name="keywords"
+          content="タイピングゲーム, タイピング練習, 対戦, 冒険, 無料"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="タイピング冒険記" />
+        <meta
+          property="og:description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta property="og:site_name" content="タイピング冒険記" />
+        <meta property="og:url" content="https://typing-adventure.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="タイピング冒険記" />
+        <meta
+          name="twitter:description"
+          content="タイピングしながら敵とバトルして冒険していく無料のタイピングゲームです。面白い対戦をしながら練習にもなります"
+        />
+        <meta name="twitter:site" content="@sotetu79" />
+        <meta name="twitter:creator" content="@sotetu79" />
+        <meta
+          name="twitter:image"
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}/twitter.png`}
+        />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}/opengraph.png`}
+        />
+      </Head>
       <main>
         <div className="relative px-8 pt-16 pb-12 flex flex-col bg-base min-h-screen shadow-xl">
           <div className="block bg-orange-300 mb-3 rounded-lg p-1 sm:hidden">
