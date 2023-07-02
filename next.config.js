56c5743a241a2bd7e56ab512365de929f3a3cfc4
sotetu-79 <sotetu79@gmail.com>
2023-07-02 15:36:44 +0900

    メタデータの修正

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "typing-adventure.com"],
   },
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://typing-adventure.com" : "",
 };
 
 module.exports = nextConfig;
