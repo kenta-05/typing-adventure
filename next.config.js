f1d1242e15c4781a59136685d91cb84b956717ab
sotetu-79 <sotetu79@gmail.com>
2023-07-02 14:21:03 +0900

    設定ファイルを修正

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "typing-adventure.com"],
   },
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://typing-adventure.com" : "",
 };
 
 module.exports = nextConfig;
