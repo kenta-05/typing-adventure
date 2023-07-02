bce280446124a86b5b063ec228a56c99c1403865
sotetu-79 <sotetu79@gmail.com>
2023-07-02 21:27:47 +0900

    設定ファイルの再修正

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     domains: ["lh3.googleusercontent.com", "typing-adventure.com"],
   },
   assetPrefix: "https://typing-adventure.com",
+  env: {
+    NEXT_PUBLIC_BASE_URL: "https://typing-adventure.com",
+  },
 };
 
 module.exports = nextConfig;
